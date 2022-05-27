9ad7d7ece6bea5e030139c1273470e67be39da02
assimdll <a.diallo_etu@enset-media.ac.ma>
2022-05-27 19:19:52 +0100

    committt

--- db.py
+++ db.py
@@ -1,40 +1,25 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri May  6 21:39:01 2022
-
-@author: USER
-"""
-
-##Step 1
-import pymysql
 import mysql.connector
-
-## Step 2
+# faire quelque chose d'utile avec la connexion
 try:
-    # Connect to the database
-    connection = pymysql.connect(host='localhost',
-                             user='root',
-                             password='',
-                             db='demo')
+
+    db = mysql.connector.connect(
+    host="",
+    user="root",
+    password="",
+    database="demo"
+)
+
 ## Step3
-    cursor=connection.cursor()
+    cursor=db.cursor()
     # Create a new record
     sql = "INSERT INTO `employe` (`ID`, `name`) VALUES (%s, %s)"
-    cursor.execute(sql, (1009,'Morgan'))
-    # connection is not autocommit by default. So we must commit to save our changes.
-    connection.commit()
-## Step 4
-    # Execute query
-    sql = "SELECT * FROM `employe`"
-    cursor.execute(sql)
-    # Fetch all the records
-    result = cursor.fetchall()
-    for i in result:
-        print(i)
-#except Error as e:
-  
-## Step 5
+    cursor.execute(sql, (1,'Le D'))
+    db.commit()
+
 finally:
-    # close the database connection using close() method.
-   # connection.close()
-   pass
+    pass
+
+
+
+
+db.close()
